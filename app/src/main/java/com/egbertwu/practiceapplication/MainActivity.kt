a31c4a0c928425d40c53bc5b5f36736d76ddf546
egbert-wu <egbert.wu@swag.live>
2020-11-26 18:15:36 +0800

    Add toolboar navigation support

--- app/src/main/java/com/egbertwu/practiceapplication/MainActivity.kt
+++ app/src/main/java/com/egbertwu/practiceapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.egbertwu.practiceapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+    
 }
